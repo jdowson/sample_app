6aa89553214f40495a49c8f84680cf319aaf310d
James A Dowson <james_a_dowson@hotmail.com>
2010-10-22 15:03:22 +0100

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -57,4 +57,20 @@ describe PagesController do
   end
 
 
+  describe "GET 'help'" do
+
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title",
+                               "Ruby on Rails Tutorial Sample App | Help")
+    end
+  
+  end
+
+
 end
